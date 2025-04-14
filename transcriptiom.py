5422afd0118fe9103957a76ee43c202a7e149be8
DhaaraniTummala <138603539+DhaaraniTummala@users.noreply.github.com>
2025-04-14 11:47:13 +0530

    Add files via upload

---
+++ transcriptiom.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+recognizer = sr.Recognizer()
+
+
+audio_file = "C:/Users/HP/Desktop/dhaarani/code/python/Rhyme.wav"
+
+with sr.AudioFile(audio_file) as source:
+    print("Listening to the audio...")
+    audio = recognizer.record(source)
+
+try:
+    text = recognizer.recognize_google(audio, language="te-IN")
+    print("Transcription in Telugu: ", text)
+
+    with open("telugu_transcription.txt", "w", encoding="utf-8") as file:
+        file.write(text)
+
+    print("Transcription saved to telugu_transcription.txt")
+
+except sr.RequestError:
+    print("Could not request results from Google Speech Recognition service")
+except sr.UnknownValueError:
+    print("Audio could not be understood")
